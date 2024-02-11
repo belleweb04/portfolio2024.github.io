56e7dd016d96c1c9d68770b02b35e71deb49d639
belleweb04 <82039049+belleweb04@users.noreply.github.com>
2024-02-11 16:43:03 +0900

    Add files via upload

---
+++ swiper-detail.js
@@ -0,0 +1,13 @@
+<script>
+  var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+  });
+</script>
